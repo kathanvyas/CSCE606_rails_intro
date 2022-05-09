af165600eb5a889fbdf55ecf73e0e915671f0b2f
Kathan Vyas <30324812+kathanvyas@users.noreply.github.com>
2022-05-08 21:26:46 -0500

    Add files via upload
    remote upload v2

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,60 @@
+class MoviesController < ApplicationController
+
+    def show
+      id = params[:id] # retrieve movie ID from URI route
+      @movie = Movie.find(id) # look up movie by unique ID
+      # will render app/views/movies/show.<extension> by default
+    end
+  
+    def index
+      @all_ratings = Movie.rating
+      @sorted_movies = params[:sort]||session[:sort]
+      session[:ratings] = session[:ratings]|| Hash[@all_ratings.map {|rating| [rating, rating]}]
+      @filter =  params[:ratings]  || session[:ratings]
+      session[:sort]= @sorted_movies
+      session[:ratings]= @filter
+      @movies = Movie.where(rating:session[:ratings].keys).order(session[:sort])
+      
+      if params[:sort]!= session[:sort] or params[:ratings]!=session[:ratings]
+        flash.keep
+        #redirect_to movies_path(sort: session[:sort] , ratings : session[:ratings])
+        redirect_to :sort => session[:sort], :ratings => session[:ratings] and return
+      end
+     
+    end
+  
+    def new
+      # default: render 'new' template
+    end
+  
+    def create
+      @movie = Movie.create!(movie_params)
+      flash[:notice] = "#{@movie.title} was successfully created."
+      redirect_to movies_path
+    end
+  
+    def edit
+      @movie = Movie.find params[:id]
+    end
+  
+    def update
+      @movie = Movie.find params[:id]
+      @movie.update_attributes!(movie_params)
+      flash[:notice] = "#{@movie.title} was successfully updated."
+      redirect_to movie_path(@movie)
+    end
+  
+    def destroy
+      @movie = Movie.find(params[:id])
+      @movie.destroy
+      flash[:notice] = "Movie '#{@movie.title}' deleted."
+      redirect_to movies_path
+    end
+  
+    private
+    # Making "internal" methods private is not required, but is a common practice.
+    # This helps make clear which methods respond to requests, and which ones do not.
+    def movie_params
+      params.require(:movie).permit(:title, :rating, :description, :release_date)
+    end
+  end
